905b3892787296c1b8b7db02cc18a0d1606e2c18
shruti32 <shruti.agrawal92@gmail.com>
2018-08-20 00:42:45 +0530

    Code

--- Sort list of strings.py
+++ Sort list of strings.py
@@ -26,6 +26,17 @@ print(split_list(list1))
 list2 = spt_char
 print(list2)
 
+list_sorted = list()
+def sorted_string(list2, list1):
+    for i in range(len(list2)):
+        for j in range(i+1, len(list2)):
+            while(len(list2[i])<len(list2[j])):
+                list2[i].append('0')
+                print(list2)
+            if(list2[i][0]<list2[j][0]):
+                list_sorted.append(list1[i])
+            elif(list2[i][0]==list2[j][0] and list2[i][1]<list2[j][1]):
+                list_sorted.append(list1[i])#this will go a long way, I need your guidance here
+    return sorted_string
 
-
-
+print(sorted_string(list2, list1))
