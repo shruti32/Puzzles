1c03ba2d3b1cccb0e2304aecafdeb984c864558e
shruti32 <shruti.agrawal92@gmail.com>
2018-08-21 00:27:00 +0530

    Modifications

--- Sort list of strings.py
+++ Sort list of strings.py
@@ -5,13 +5,16 @@ Created on Sun Aug 12 17:32:33 2018
 @author: shrut
 """
 list1 = ["1.0.0","1","1.1.0","1.1","1.0","1.1.1","1.1.1.0"]
+
+list_len = list()
 def length_list(list1):
-    list_len = list()
     for i in range(len(list1)):
         list_len.append(len(list1[i]))
         print(list_len)
     return length_list
 print(length_list(list1))
+
+print(list_len)
 
 spt_char = list()
 def split_list(list1):
@@ -27,15 +30,25 @@ list2 = spt_char
 print(list2)
 
 list_sorted = list()
-def sorted_string(list2, list1):
+def sorted_string(list2, list1, list_len):
     for i in range(len(list2)):
         for j in range(i+1, len(list2)):
+            #two while loops to make strings equal in length
             while(len(list2[i])<len(list2[j])):
                 list2[i].append('0')
                 print(list2)
-            if(list2[i][0]<list2[j][0]):
+            while(len(list2[i])>len(list2[j])):
+                list2[j].append('0')
+                #sort the strings
+            if(list2[i][0:len(list2[i])]<list2[j][0:len(list2[j])]):
                 list_sorted.append(list1[i])
-            elif(list2[i][0]==list2[j][0] and list2[i][1]<list2[j][1]):
+                #we will compare the characters at each position and at the instant the
+                #character of one string is greater than other we will assign that element first place
+            elif(list2[i][0:len(list2[i])]==list2[j][0:len(list2[j])]):
+                if(list_len[i]<list_len[j]):
+                    list_sorted.append(list1[i])
+                else:
+                    list_sorted.append(list1[j])
                 list_sorted.append(list1[i])#this will go a long way, I need your guidance here
     return sorted_string
 
