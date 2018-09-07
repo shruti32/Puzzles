a718d0ff63eaa1de8174f0afadd917c0d0d935b9
shruti32 <shruti.agrawal92@gmail.com>
2018-09-07 09:26:35 +0530

    Comments

--- Sort list of strings.py
+++ Sort list of strings.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 
+#Splitting the elements of the list
 def split_list(list1):
     spt_char = list()
     for i in range(len(list1)):
@@ -9,6 +10,7 @@ def split_list(list1):
     print(spt_char)
     return spt_char
 
+#Creating a list of length of all the elements of the list
 def length_list(spt_char):
     list_len = list()
     for i in range(len(spt_char)):
@@ -20,6 +22,7 @@ list1 = ["1.0.0","1","1.1.0","1.1","1.0","1.1.1","1.1.1.0"]
 s = split_list(list1)
 l = length_list(s)
 
+#defining the range of keys for the dictionary
 dict_key_max_len = len(max(s, key=len))
 for i in range(len(s)):
     while (len(s[i]) < dict_key_max_len):
@@ -27,17 +30,16 @@ for i in range(len(s)):
 
 dict_key = list(range(0, dict_key_max_len))
 
+#creating a dictionary with none value
 keys = {k:None for k in dict_key}
 
+#Creating a dictionary with all the components of elements of a list and lengths of the elements
 dict = OrderedDict()
-
 i=0
-
 for key in keys:
     for element in s:
         dict.setdefault(key, []).append(element[i])
     i += 1
-
 dict['element_length'] = l
 
 #Sorting is pending
